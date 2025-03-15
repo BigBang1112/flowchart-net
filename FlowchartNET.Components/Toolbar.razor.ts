090ed649a461bbbe3debce67ff3ab324f5cb4bad
Petr Pivoňka <petrpiv1@gmail.com>
2025-03-15 04:05:47 +0100

    Add light theme

---
+++ FlowchartNET.Components/Toolbar.razor.ts
@@ -0,0 +1,8 @@
+﻿export function toggleTheme(toggle: boolean): void {
+    if (toggle) {
+        document.body.classList.add("light");
+    }
+    else {
+        document.body.classList.remove("light");
+    }
+}
