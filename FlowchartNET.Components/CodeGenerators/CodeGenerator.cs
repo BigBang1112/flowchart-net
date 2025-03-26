58ab2cf86844fccdffb5ac8bf9449ba8b6639d97
Petr Pivoňka <petrpiv1@gmail.com>
2025-03-26 01:55:26 +0100

    Add basic basic code gen

---
+++ FlowchartNET.Components/CodeGenerators/CodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using FlowchartNET.Components.Symbols.Data;
+using System.Text;
+
+namespace FlowchartNET.Components.CodeGenerators;
+
+internal abstract class CodeGenerator
+{
+    public string Generate(StartSymbolData startSymbol)
+    {
+        var sb = new StringBuilder();
+        GenerateSymbolCode(sb, startSymbol);
+        return sb.ToString();
+    }
+
+    protected abstract void GenerateSymbolCode(StringBuilder sb, StartSymbolData startSymbol);
+}
