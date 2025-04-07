12d599aa6dd4ded60c53ae85558af868e3603345
Petr Pivoňka <petrpiv1@gmail.com>
2025-04-07 23:30:31 +0200

    Make delete general button

--- FlowchartNET.Components/Symbols/ISymbol.cs
+++ FlowchartNET.Components/Symbols/ISymbol.cs
@@ -5,12 +5,12 @@ namespace FlowchartNET.Components.Symbols;
 
 public interface ISymbol
 {
-    public Guid Id { get; }
-    public bool IsDragging { get; set; }
-    public double X { get; set; }
-    public double Y { get; set; }
-    public SymbolData Data { get; }
-    public ElementReference Shape { get; }
-    public string? GetSocketIn();
-    public string? GetSocketOut(Guid nextSymbolId);
+    Guid Id => Data.Id;
+    double X { get => Data.X; set => Data.X = value; }
+    double Y { get => Data.Y; set => Data.Y = value; }
+    bool IsDragging { get; set; }
+    SymbolData Data { get; }
+    ElementReference Shape { get; }
+    string? GetSocketIn();
+    string? GetSocketOut(Guid nextSymbolId);
 }
